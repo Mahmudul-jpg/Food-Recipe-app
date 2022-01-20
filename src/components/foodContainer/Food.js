33d2cf2619b58ce6937a7060b032bc705d089a8e
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-20 17:42:40 +0600

    added styling on card

--- src/components/foodContainer/Food.js
+++ src/components/foodContainer/Food.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import FoodCard from '../FoodCard/FoodCard'
+import "./style.css"
 function Food({ meals }) {
-    return <div>{meals.map((meal) => (<FoodCard />))}</div>;
+    return <div className="meals-container">{meals.map((meal) => (<FoodCard {...meal} />))}</div>;
 }
 
 export default Food;
