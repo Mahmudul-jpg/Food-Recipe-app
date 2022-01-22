0a0e67c4ba82f1057d3dbd2fee238920a99e59fb
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-22 12:57:54 +0600

    added route on pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 import Appbar from './components/navbar/Appbar'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Context from './Context'
+import ErrorPage from './pages/ErrorPage'
+import SignUp from './pages/SignUp'
 function App() {
 
   return (
     <Context>
-      <div >
-        <Appbar />
-        <Home />
-      </div>
+
+
+
+      <Appbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes >
     </Context>
   );
 }
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ErrorPage() {
+    return <div>
+        <h1><span>404:</span></h1>  the page not found or the page is private
+    </div>;
+}
+
+export default ErrorPage;
